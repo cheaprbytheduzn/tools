078aabed43e4086c99fbaee0bb3ba1040845ab6e
Ken Miles <cheaprbytheduzn@gmail.com>
2014-02-12 07:29:54 -0800

    Add ntp

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Install any packages we find useful in a base machine:
 
-tools = %w( lsof nmap tree )
+tools = %w( lsof nmap tree ntp )
 
 if node['platform_family'] == 'rhel'
   tools << 'dos2unix'
@@ -17,4 +17,9 @@ end
 
 tools.each do |pkg|
   package pkg
+end
+
+service "ntpd" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
 end
